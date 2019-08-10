00eca87cb287296f218181267dd2e89001ea6fa3
orekyuu <orekyuu@gmail.com>
2019-08-10 11:41:16 +0900

    テストを通す

--- kata1_springcore/src/main/java/net/orekyuu/springbootkata/component/CountUpService.java
+++ kata1_springcore/src/main/java/net/orekyuu/springbootkata/component/CountUpService.java
@@ -1,8 +1,11 @@
 package net.orekyuu.springbootkata.component;
 
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
+@Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
 public class CountUpService {
 
     private int count;
