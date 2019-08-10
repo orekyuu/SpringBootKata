f14a7e3fa0b10d17ff57d55ebb2808ec54ed995f
orekyuu <orekyuu@gmail.com>
2019-08-10 14:42:15 +0900

    テストを通した

--- kata1_springcore/src/main/java/net/orekyuu/springbootkata/component/MessageConfiguration.java
+++ kata1_springcore/src/main/java/net/orekyuu/springbootkata/component/MessageConfiguration.java
@@ -1,10 +1,12 @@
 package net.orekyuu.springbootkata.component;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class MessageConfiguration {
+    @Value("${kata.message}")
     private String message = "default";
 
     @Bean
