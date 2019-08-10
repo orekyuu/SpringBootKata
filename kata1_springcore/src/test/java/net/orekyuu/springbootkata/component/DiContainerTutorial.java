a16b077dc906f97ae3a69cffcdb7fbdca1fd61c3
orekyuu <orekyuu@gmail.com>
2019-08-10 11:26:48 +0900

    不要なimportを消す

--- kata1_springcore/src/test/java/net/orekyuu/springbootkata/component/DiContainerTutorial.java
+++ kata1_springcore/src/test/java/net/orekyuu/springbootkata/component/DiContainerTutorial.java
@@ -2,10 +2,8 @@ package net.orekyuu.springbootkata.component;
 
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
