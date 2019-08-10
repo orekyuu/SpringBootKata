7cc77e2374a652de38e4aaf8a9c34f878fbc1b9b
orekyuu <orekyuu@gmail.com>
2019-08-10 11:22:57 +0900

    nestedなクラスでautowiredが動かなかった問題を修正

--- kata1_springcore/src/main/java/net/orekyuu/springbootkata/TutorialApplication.java
+++ kata1_springcore/src/main/java/net/orekyuu/springbootkata/TutorialApplication.java
@@ -1,7 +1,12 @@
 package net.orekyuu.springbootkata;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class TutorialApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TutorialApplication.class, args);
+    }
 }
